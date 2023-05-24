9173f41890595a3743cf7f60a14e704f026078b2
Faizan Jan <faizanjan619@gmail.com>
2023-05-24 14:53:38 +0530

    Add animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,62 @@
 import { useEffect, useState } from "react";
 
-let Card = ({ id, value, gameStarted, handleCardClick, doFlip, paired}) => {
+let Card = ({ id, value, gameStarted, handleCardClick, doFlip, paired }) => {
   let [isActive, setIsActive] = useState(true);
+  let [triggerFlip, setFlip] = useState(false);
+  let [triggerFlipBack, setFlipBack] = useState(false);
+
   useEffect(() => {
     setTimeout(() => {
-      setIsActive(false);
+        setIsActive(false);
+      setFlip(true);
+      console.log("Initial Flip")
+      setTimeout(() => {
+        setFlip(false);
+      console.log("Initial Flip reset to false")
+      }, 350);
     }, 2000);
   }, []);
 
   useEffect(() => {
-    if(doFlip) setIsActive(false);
+    if (doFlip) {
+      setFlip(true);
+      console.log("mismatch Flip")
+      setTimeout(() => {
+        setFlip(false);
+        console.log("mismatch Flip reset")
+    }, 350);
+      setIsActive(false);
+    }
   }, [doFlip]);
 
   let handleClick = () => {
     if (!isActive && gameStarted) {
-      setIsActive(true);
-      handleCardClick(id, value);
+      setFlipBack(true);
+      console.log("click Flip")
+      setTimeout(() => {
+        setFlipBack(false);
+        console.log("click Flip reset")
+    }, 350);
+    setIsActive(true);
+    handleCardClick(id, value);
     }
   };
 
+  const cardStyles = {
+    animation: `${
+      triggerFlip
+        ? "flip 0.3s linear"
+        : triggerFlipBack
+        ? "flipBack 0.3s linear"
+        : "none"
+    }`,
+    background: `${
+      paired ? "linear-gradient(transparent, rgb(0, 0, 0, 0.3))" : ""
+    }`,
+  };
+
   return (
-    <div
-      className="card"
-      id={id}
-      onClick={handleClick}
-        style={{ background: `${paired ? "linear-gradient(transparent, rgb(0, 0, 0, 0.3))" : ""}` }}
-    >
+    <div className="card" id={id} onClick={handleClick} style={cardStyles}>
       {isActive && (
         <span className="card-value" style={{ position: "absolute" }}>
           {value}
